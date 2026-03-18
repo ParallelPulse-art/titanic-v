4f3a520d6deaf01d6807e5e92c17b40c00d99bb2
ParallelPulse-art <anr59481@gmail.com>
2026-03-18 11:00:12 +0530

    Update page title in Titanic ML Dashboard

--- titanic_app.py
+++ titanic_app.py
@@ -16,7 +16,7 @@ warnings.filterwarnings("ignore")
 # PAGE CONFIG
 # ─────────────────────────────────────────
 st.set_page_config(
-    page_title="Titanic ML Dashboard",
+    page_title="Titanic ",
     page_icon="🚢",
     layout="wide",
     initial_sidebar_state="expanded"
